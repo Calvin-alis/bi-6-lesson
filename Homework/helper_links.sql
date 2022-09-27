1dbdb46475f0ce6d4f87e354c1b090b958385fc6
Calvin-alis <alis.demiurg@gmail.com>
2022-09-27 17:49:45 +0300

    Help link

---
+++ Homework/helper_links.sql
@@ -0,0 +1,18 @@
+/*
+Додаткові курси:
+	https://learn-anything.xyz/programming/programming-languages/sql
+	https://www.coursera.org/
+	https://welcome.stepik.org/ru
+	https://www.w3schools.com/sql/default.asp
+
+
+Практика:
+	https://www.w3schools.com/sql/default.asp 
+	https://www.codewars.com/dashboard
+	https://www.hackerrank.com/domains/sql?filters%5Bsubdomains%5D%5B%5D=select
+	http://sql-tutorial.ru/sqlbook/ru
+	https://sql-ex.ru
+*/
+
+
+
